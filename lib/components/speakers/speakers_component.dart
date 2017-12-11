1b71b7c797d5023da0612d4fc6862c0250fc7cff
Quirijn <quirijn.grootbluemink@gmail.com>
2017-12-11 21:36:05 +1100

    Display skill tags

--- lib/components/speakers/speakers_component.dart
+++ lib/components/speakers/speakers_component.dart
@@ -3,7 +3,6 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class SpeakersPage extends StatefulWidget {
-
   @override
   _SpeakersPagePageState createState() => new _SpeakersPagePageState();
 }
@@ -20,7 +19,7 @@ class _SpeakersPagePageState extends State<SpeakersPage> {
             query: reference,
             sort: (a, b) => b.key.compareTo(a.key),
             padding: new EdgeInsets.all(8.0),
-            reverse: true,
+            reverse: false,
             itemBuilder:
                 (_, DataSnapshot snapshot, Animation<double> animation) {
               return new SpeakerItem(snapshot: snapshot, animation: animation);
@@ -40,21 +39,65 @@ class SpeakerItem extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return new SizeTransition(
-      sizeFactor: new CurvedAnimation(parent: animation, curve: Curves.easeOut),
-      axisAlignment: 0.0,
-      child: new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            new Image.network("https://hoverboard-demo.firebaseapp.com" + snapshot.value['photoUrl']),
-            new ListTile(
-              title: new Text(snapshot.value['name']),
-              subtitle: new Text(snapshot.value['country']),
-            ),
-          ],
-        ),
-      )
+        sizeFactor:
+            new CurvedAnimation(parent: animation, curve: Curves.easeOut),
+        axisAlignment: 0.0,
+        child: new Card(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Image.network("https://hoverboard-demo.firebaseapp.com" +
+                  snapshot.value['photoUrl']),
+              new ListTile(
+                title: new Text(snapshot.value['name']),
+                subtitle: new Text(snapshot.value['country']),
+              ),
+              new SpeakerSkillsItem(snapshot.value['tags']),
+            ],
+          ),
+        ));
+  }
+}
+
+class SpeakerSkillsItem extends StatelessWidget {
+  SpeakerSkillsItem(this.skill);
+
+  final List skill;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = <Widget>[];
+    for (String tag in skill) {
+      widgets.add(new SpeakerSkillItem(tag));
+    }
+    return new Container(
+      margin: new EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 8.0),
+      child: new Row(
+        children: widgets,
+      ),
     );
   }
 }
 
+class SpeakerSkillItem extends StatelessWidget {
+  SpeakerSkillItem(this.skill);
+
+  final String skill;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      decoration: new BoxDecoration(
+        color: Colors.red,
+        border: new Border.all(width: 1.0, color: Colors.red),
+        borderRadius: const BorderRadius.all(const Radius.circular(4.0)),
+      ),
+      padding: new EdgeInsets.all(4.0),
+      margin: new EdgeInsets.all(4.0),
+      child: new Text(
+        skill,
+        style: new TextStyle(color: Colors.white, fontSize: 10.0),
+      ),
+    );
+  }
+}
